d6aa6d8677c13fbfa4ba4487e9ca9260efd454ed
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-08-25 13:36:47 -0300

    modificacion part A

--- participanteA-archivoA.js
+++ participanteA-archivoA.js
@@ -2,5 +2,7 @@ console.log("Este es el archivo 1 del participante A");
 
 console.log("Archivo modificado por el participante B");
 
-console.log("Paso 8 - Resolviendo conflictos")
+console.log("Paso 8 - Resolviendo conflictos");
 console.log("Modificando el archivo para ver que onda...");
+
+console.log("La modificación mas esperada del 2022.");
